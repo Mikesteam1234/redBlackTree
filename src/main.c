8332254a9531a866edfa03f82eb60c72cec18ab7
Michael Remley <Mikejamesremley@gmail.com>
2024-02-06 11:14:54 -0800

    Add src folder and main.c

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/* Author: 	Michael Remley
+ * Email:  	Mikejamesremley@gmail.com
+ * Date:	Feb 6th 2024
+ * 
+ * Project Details:
+ * Red Black tree written in C. 
+ * */
+
+#include <stdio.h>
+
+int main(int argc, int[] argv) {
+
+	printf("Hello World!");
+
+	return 0;
+}
